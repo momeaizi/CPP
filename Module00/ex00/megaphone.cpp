52ea63024fa690913016e1f7ae18a36723753538
Mohammed taha Meaizi <momeaizi@e2r5p10.1337.ma>
2022-11-15 13:01:54 +0100

    ex00

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momeaizi <momeaizi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 13:00:35 by momeaizi          #+#    #+#             */
+/*   Updated: 2022/11/15 13:01:30 by momeaizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+void	megaphone(char *str);
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < ac; i++)
+		megaphone(av[i]);
+	std::cout << std::endl;
+}
+
+void	megaphone(char *str)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (islower(str[i]))
+			str[i] -= 32;
+		std::cout << str[i];
+	}
+}
